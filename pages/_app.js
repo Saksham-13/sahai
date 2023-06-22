0f6d487b30abfa2927640acd3ec889c2392cb6d8
Saksham-13 <sakshamalok13@gmail.com>
2023-06-22 19:53:34 +0530

    adds favicon and title

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import Layout from '../components/layout';
+import Head from 'next/head';
 import '../styles/globals.css';
 import { ParallaxProvider } from 'react-scroll-parallax';
 function MyApp({ Component, pageProps }) {
   return (
     <ParallaxProvider>
+      <Head>
+        <title>Sahai India</title>
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='shortcut icon' href='' />
+        </Head>
        <Layout>
       <Component {...pageProps} />
     </Layout>
