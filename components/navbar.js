198916996aad6a19dbfdcca19102d894e995a9a6
Saksham-13 <sakshamalok13@gmail.com>
2023-06-22 20:23:06 +0530

    no

--- components/navbar.js
+++ components/navbar.js
@@ -51,7 +51,7 @@ function NavLink({ to, children, dropdown }) {
 function MobileNav({ open, setOpen }) {
   return (
     <div
-      className={`absolute top-0 left-0 h-screen w-screen bg-[#435559]   transform ${
+      className={`absolute top-0 left-0 h-screen pb-4 w-screen bg-[#435559]   transform ${
         open ? "-translate-x-0" : "-translate-x-full"
       } transition-transform duration-300 ease-in-out filter drop-shadow-md `}
     >
